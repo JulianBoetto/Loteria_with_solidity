b3baf842756bc761d4f2f8ec0e07a8e992886e6f
Julián Boetto <julib_8724@hotmail.com>
2022-08-31 22:09:01 +0200

    Pick winner function OK

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,24 +6,53 @@ pragma solidity ^0.8.1;
 
 contract Lottery {
     address public immutable OWNER;
-
-    address[] public players; 
+    address[] public players;
+    uint256 private counter;
 
     constructor() {
         OWNER = msg.sender;
     }
 
     function enter() public payable {
-        require(msg.value == 0.1 ether, "Invalid amount. Please, send 0.1 Ether"); 
+        require(
+            msg.value == 0.1 ether,
+            "Invalid amount. Please, send 0.1 Ether"
+        );
 
         players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.timestamp,
+                        block.difficulty,
+                        players,
+                        counter
+                    )
+                )
+            );
+    }
+
+    function pickWinner() public onlyOwner returns (address payable) {
+        address payable winner = payable(players[random() % players.length]);
+
+        winner.transfer(address(this).balance);
+
+        players = new address[](0);
+
+        counter = counter + 1;
+
+        return winner;
     }
 
     function getPlayers() public view returns (address[] memory) {
         return players;
     }
 
-    modifier onlyOwner {
+    modifier onlyOwner() {
         require(OWNER == msg.sender, "Only Owner");
         _;
     }
