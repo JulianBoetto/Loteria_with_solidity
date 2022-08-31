688b955b72c16e62b06f7c979dadbdaa1c29bf45
Julián Boetto <julib_8724@hotmail.com>
2022-08-31 22:30:49 +0200

    Check balance OK

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -8,6 +8,20 @@ contract("Lottery", function ([accA, accB, accC]) {
     it("Verify owner", async () => {
         const ownerAddress = await contract.OWNER();
 
-        assert(ownerAddress == accA, "Need to be equal")
-    })
+        assert(ownerAddress === accA, "Need to be equal")
+    });
+
+    it("Can get players", async () => {
+        const players = await contract.getPlayers();
+
+        assert(players.length === 0, "Need to be zero")
+    });
+
+    it("Can check balance ", async () => {
+        const contractAddress = await contract.address;
+        const balance = await web3.eth.getBalance(contractAddress);
+        const etherBalance = web3.utils.fromWei(balance, "ether");
+
+        assert(etherBalance === "0", "Need to be equal")
+    });
 })
