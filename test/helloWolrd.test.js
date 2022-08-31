95b5c2f67c601a16d1965fdc5578365a7ea72515
Julián Boetto <julib_8724@hotmail.com>
2022-08-31 14:35:47 +0200

    First test passing

--- test/helloWolrd.test.js
+++ test/helloWolrd.test.js
@@ -1,5 +1,13 @@
 const HelloWorld = artifacts.require("HelloWorld");
 
 contract('HelloWorld', function(accounts) {
- 
+  beforeEach(async () => {
+    contract = await HelloWorld.new();
+  });
+
+  it("need show message", async () => {
+    const res = await contract.showMessage();
+
+    assert(res === "Hello World!");
+  });
 });
