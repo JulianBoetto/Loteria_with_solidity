7bc595c9893e29c301d14c86c753a4e99162f164
Julián Boetto <julib_8724@hotmail.com>
2022-09-01 14:03:31 +0200

    Network test binance OK

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # First Smart Contract with Solidity - Truffle - Node JS
 
+For more BNB Test: https://testnet.binance.org/faucet-smart
+
 2022 @ Donations address: 0x808ee9973ff652b3Bd2Df33A2eF1AE182335f3f6
--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  networks: {},
+  networks: {
+    bscTest: {
+      host: "https://data-seed-prebsc-1-s1.binance.org",
+      port: 8545,
+      network_id: "97" // Match any network id
+    }
+  },
   compilers: {
     solc: {
       version: "0.8.1",
